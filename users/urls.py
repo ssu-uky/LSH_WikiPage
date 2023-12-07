a0029595e231241e8404899ede3fee4f18716f23
ssu-uky <id_suhyun@naver.com>
2023-12-07 21:01:57 +0900

    💡 chore : user url 이름 수정

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
